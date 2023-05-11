a19b8293e27417651a7eae218599cbdea4459ea5
barisTas <baris.tas@teloseng.com>
2023-05-11 16:50:01 +0300

    producer and consumer implemented for hello world program

---
+++ HW/RabbitMqConsumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMqConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://zgvcyqrk:FAAZd2v-1z-jl4vKNUoizuh_XFrxKHcR@hawk.rmq.cloudamqp.com/zgvcyqrk");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += MessageReceived;
+
+            //Lambda implementation of receive slot
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    
+            //};
+
+            Console.ReadLine();
+        }
+
+        private static void MessageReceived(object? sender, BasicDeliverEventArgs e)
+        {
+            var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            Console.WriteLine("message received: " + msg);
+        }
+    }
+}
---
+++ HW/RabbitMqPublisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://zgvcyqrk:FAAZd2v-1z-jl4vKNUoizuh_XFrxKHcR@hawk.rmq.cloudamqp.com/zgvcyqrk");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false, null);
+
+            string message = "hello world";
+
+            var msgBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, msgBody);
+
+            Console.WriteLine("msg sent");
+
+            Console.ReadLine();
+        }
+    }
+}
