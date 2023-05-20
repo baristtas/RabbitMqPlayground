6b70c48a7eb9a7c50e01c19083ade8b638ade293
Baris TAS <baristtas@gmail.com>
2023-05-20 23:21:05 +0300

    Direct exchange implemented

---
+++ Exchanges/Direct Exchange/RabbitMqConsumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+
+namespace RabbitMqConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var queueName = "direct-exchangeCritical";
+
+            Console.WriteLine("listening " + queueName);
+            var consumer = new EventingBasicConsumer(channel);
+
+
+            channel.QueueBind(queueName, "logs-direct", queueName, null);
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            //Lambda implementation of receive slot
+            consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1000);
+                Console.WriteLine("message received: " + msg);
+                File.AppendAllText("logs-critical.txt", msg + "\n");
+                channel.BasicAck(e.DeliveryTag,true);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exchanges/Direct Exchange/RabbitMqPublisher/Program.cs
@@ -0,0 +1,65 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqPublisher
+{
+    internal class Program
+    {
+        public enum LogNames
+        {
+            Critical = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4
+        };
+
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var routeKey = $"route-{x}";
+
+                var queueName = $"direct-exchange{x}";
+                channel.QueueDeclare(queueName, true, false, false, null);
+                channel.QueueBind(queueName,"logs-direct", routeKey);
+            }
+            );
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames logName = (LogNames) new Random().Next(1, 5);
+
+                string message = $"log-type: {logName}-{x}";
+
+                var msgBody = Encoding.UTF8.GetBytes(message);
+
+                var routeKey = $"route-{logName}";
+
+                channel.BasicPublish("logs-direct",routeKey,null,msgBody);
+
+                Console.WriteLine($"msg sent: {message}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
