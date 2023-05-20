25cb4fda85a28e05ca480012ac4d0c9296246ad3
Baris TAS <baristtas@gmail.com>
2023-05-20 13:47:35 +0300

    Implementing Fanout Exchange

---
+++ Exchanges/Fanout Exchange/RabbitMqConsumer/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+
+namespace RabbitMqConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var randomQueueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(randomQueueName, "logs-fanout", string.Empty, null);
+
+
+            Console.WriteLine("listening " + randomQueueName);
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicQos(0, 6, false); 
+
+            channel.BasicConsume(randomQueueName, false, consumer);
+
+            //Lambda implementation of receive slot
+            consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1000);
+                Console.WriteLine("message received: " + msg);
+                channel.BasicAck(e.DeliveryTag,true);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exchanges/Fanout Exchange/RabbitMqPublisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"FMessage {x}";
+
+                var msgBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-fanout","", null, msgBody);
+
+                Console.WriteLine($"msg sent: {message}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
