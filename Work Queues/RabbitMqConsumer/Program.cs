ee05c22486559687cfddcdd118d1a92cdd7bbcdd
Baris TAS <baristtas@gmail.com>
2023-05-20 13:16:10 +0300

    Work Queues implemented

---
+++ Work Queues/RabbitMqConsumer/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+
+namespace RabbitMqConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicQos(0, 6, false); 
+
+            channel.BasicConsume("hello-queue", false, consumer);
+
+            //Lambda implementation of receive slot
+            consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1000);
+                Console.WriteLine("message received: " + msg);
+                channel.BasicAck(e.DeliveryTag,true);
+            };
+
+            Console.ReadLine();
+        }
+
+        //private static void MessageReceived(object? sender, BasicDeliverEventArgs e)
+        //{
+        //
+        //       
+        //}
+    }
+}
---
+++ Work Queues/RabbitMqPublisher/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false, null);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+
+                var msgBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "hello-queue", null, msgBody);
+
+                Console.WriteLine($"msg sent: {message}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
