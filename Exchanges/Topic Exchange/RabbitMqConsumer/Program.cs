bd89359a64b5fc12f34237609407c622140be1fe
Baris TAS <baristtas@gmail.com>
2023-05-21 04:49:30 +0300

    Topic Exchange implemented.

---
+++ Exchanges/Topic Exchange/RabbitMqConsumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+
+namespace RabbitMqConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            channel.ExchangeDeclare("logs-topicExchange", durable: true, type: ExchangeType.Topic);
+
+            var routeKey = "Warning.*.Critical";
+            channel.QueueBind(queueName, "logs-topicExchange", routeKey, null);
+
+            Console.WriteLine("listening " + queueName);
+            var consumer = new EventingBasicConsumer(channel);
+
+
+            channel.QueueBind(queueName, "logs-direct", queueName, null);
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            //Lambda implementation of receive slot
+            consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1000);
+                Console.WriteLine("message received: " + msg);
+                File.AppendAllText("logs-critical.txt", msg + "\n");
+                channel.BasicAck(e.DeliveryTag,true);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exchanges/Topic Exchange/RabbitMqPublisher/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqPublisher
+{
+    internal class Program
+    {
+        public enum LogNames
+        {
+            Critical = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4
+        };
+
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("logs-topicExchange", durable: true, type: ExchangeType.Topic);
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log1 = (LogNames)new Random().Next(1, 5);
+                LogNames log2 = (LogNames)new Random().Next(1, 5);
+                LogNames log3 = (LogNames)new Random().Next(1, 5);
+
+                var routeKey = $"{log1}.{log2}.{log3}";
+
+                string message = $"log-type: {log1}.{log2}.{log3} - {x}";
+                var msgBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-topicExchange", routeKey,null,msgBody);
+
+                Console.WriteLine($"msg sent: {message}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
