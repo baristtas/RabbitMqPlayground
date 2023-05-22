90edf063f57160152c726fff52d598539a79c555
Baris TAS <baristtas@gmail.com>
2023-05-22 05:34:18 +0300

    Implementing Header Exchange

---
+++ Exchanges/Header Exchange/RabbitMqPublisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqPublisher
+{
+    internal class Program
+    {
+        public enum LogNames
+        {
+            Critical = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4
+        };
+
+        static void Main(string[] args)
+        {
+            string uri = "";
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\baris.tas\\Desktop\\rbmq\\amqpinstanceuri.txt");
+                uri = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(uri);
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            Dictionary<string,object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+
+            var properties = channel.CreateBasicProperties();
+
+            properties.Headers = headers;
+
+            channel.BasicPublish("header-exchange", "",string.Empty,null);
+
+            Console.ReadLine();
+        }
+    }
+}
