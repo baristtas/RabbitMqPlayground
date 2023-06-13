c55e8eefd2b3260ae781b731cea135112e5e5612
Baris TAS <baristtas@gmail.com>
2023-06-13 23:09:41 +0300

    Started to developing excel app

---
+++ Apps/CreateExcelFileTypeOutput/CreateExcelFile/Models/UserFile.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CreateExcelFile.Models
+{
+    public enum Status
+    {
+        Creating = 0,
+        Completed = 1000
+    }
+
+    public class UserFile
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        [NotMapped]
+        public string GetCreatedDate => CreatedDate.HasValue ? CreatedDate.Value.ToShortDateString() : "-";
+        
+    }
+}
---
+++ Apps/CreateExcelFileTypeOutput/CreateExcelFile/Program.cs
@@ -0,0 +1,82 @@
+using CreateExcelFile.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreateExcelFile
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL1"));
+
+            });
+            builder.Services.AddControllersWithViews();
+            
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+
+            }).AddEntityFrameworkStores<AppDbContext>();
+
+
+
+            var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                appDbContext.Database.Migrate();
+
+                if(!appDbContext.Users.Any()) 
+                {
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "test1"
+                    ,
+                        Email = "test1@gmail.com"
+                    }, password:"Password12*").Wait();
+
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "test2"
+,
+                        Email = "test2@gmail.com"
+                    }, password: "Password12*").Wait();
+                }
+            }
+
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            
+
+            app.Run();
+        }
+    }
+}
