cbe5c7c2feebec0d42d1db6a7c43690bf3c64e1a
Baris TAS <baristtas@gmail.com>
2023-06-24 23:23:15 +0300

    Files api controller added

---
+++ Apps/CreateExcelFileTypeOutput/CreateExcelFile/Controllers/FilesController.cs
@@ -0,0 +1,46 @@
+﻿using CreateExcelFile.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreateExcelFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FilesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Upload(IFormFile file, int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+
+            var userFile = await _context.UserFiles.FirstAsync(x=> x.Id== fileId);
+
+            var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+
+            using FileStream stream = new(path, FileMode.Create);
+
+            await file.CopyToAsync(stream);
+
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = path;
+            userFile.FileStatus = FileStatus.Completed;
+
+            await _context.SaveChangesAsync();
+
+            //signalr notfiitcation olustuurulacak.
+
+            return Ok();
+        }
+         
+    }
+}
